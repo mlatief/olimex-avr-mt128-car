3ec5cc0061a433f3a015822f0c649570983b8320
Mohamed Abdellatif <m.latief@gmail.com>
2015-05-12 20:03:40 +0300

    Added .gitignore and credits for the main game loop. Prepared for public repo push.

---
+++ README.md
@@ -0,0 +1,13 @@
+## Simple Car game for Olimex AVR-MT128
+
+Main game loop and road rendering developed originally by @TheRealDod, Nov 25, 2010
+https://gist.github.com/thedod/715269
+
+And we reused the modified version by @alesf, Dec 1, 2012
+https://gist.github.com/alesf/4186127
+
+#### Main features:
+
++ Car can jump, accelerate/deaccelerate in additional to change lanes
++ Added Sound and simplified game logic ( only two lanes )
++ High Scores are stored in EEPROM
--- car1.c
+++ car1.c
@@ -1,3 +1,15 @@
+/* Simple Car game for Olimex AVR-MT128 board
+	Main game loop and road rendering developed originally by @TheRealDod, Nov 25, 2010
+    https://gist.github.com/thedod/715269
+    
+	And we reused the modified version by @alesf, Dec 1, 2012
+    https://gist.github.com/alesf/4186127
+    
+    + Car can jump, accelerate/deaccelerate in additional to change lanes
+    + Added Sound and simplified game logic ( only two lanes )
+    + High Scores are stored in EEPROM
+*/
+
 /* car1.c - main file: game loop, sound playing, road rendering .. etc */
 #include <stdlib.h>
 #include <avr/io.h>
